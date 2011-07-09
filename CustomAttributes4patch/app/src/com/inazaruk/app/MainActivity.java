a3c5428cf6967f5ae02ca491d70d3f99b415781a
inazaruk <ievgenii.nazaruk@gmail.com>
2011-07-09 22:34:02 +0300

    Added example project that uses proposed patch for custom xml attributes in libraries.

---
+++ CustomAttributes4patch/app/src/com/inazaruk/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.inazaruk.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{    
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.libactivity);
+    }
+}
