ee3b5cfe66ef1088f11e576fecd130d74eacc587
inazaruk <inazaruk@lohika.com>
2011-11-14 19:56:46 +0200

    Added example of map fragment

---
+++ MapFragmentExample/src/com/inazaruk/example/MainFragmentActivity.java
@@ -0,0 +1,13 @@
+package com.inazaruk.example;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainFragmentActivity extends FragmentActivity {
+    
+    @Override    
+    protected void onCreate(Bundle bundle) {        
+        super.onCreate(bundle);
+        setContentView(R.layout.main_fragment_activity);        
+    }
+}
