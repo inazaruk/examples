1e69e977ea9590f56d64062aa177c266485a0f66
inazaruk <inazaruk@lohika.com>
2012-05-24 17:32:05 +0300

    Simplified MyMapFragment. Removed TabHost from implementation.

--- MapFragmentExample/src/com/inazaruk/example/LocalActivityManagerFragment.java
+++ MapFragmentExample/src/com/inazaruk/example/LocalActivityManagerFragment.java
@@ -3,10 +3,14 @@ package com.inazaruk.example;
 import android.app.LocalActivityManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
-@SuppressWarnings("deprecation")
+/**
+ * This is a fragment that will be used during transition from activities to fragments.
+ */
 public class LocalActivityManagerFragment extends Fragment {
 
+    private static final String TAG = LocalActivityManagerFragment.class.getSimpleName();
     private static final String KEY_STATE_BUNDLE = "localActivityManagerState";
     
     private LocalActivityManager mLocalActivityManager;
@@ -19,6 +23,7 @@ public class LocalActivityManagerFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(): " + getClass().getSimpleName());
         
         Bundle state = null;
         if(savedInstanceState != null) {
@@ -38,24 +43,28 @@ public class LocalActivityManagerFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+        Log.d(TAG, "onResume(): " + getClass().getSimpleName());
         mLocalActivityManager.dispatchResume();
     }
     
     @Override
     public void onPause() {
         super.onPause();
+        Log.d(TAG, "onPause(): " + getClass().getSimpleName());
         mLocalActivityManager.dispatchPause(getActivity().isFinishing());
     }    
     
     @Override
     public void onStop() {
         super.onStop();
+        Log.d(TAG, "onStop(): " + getClass().getSimpleName());
         mLocalActivityManager.dispatchStop();
     }
     
     @Override
-    public void onDestroy() {        
-        super.onDestroy();        
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy(): " + getClass().getSimpleName());
         mLocalActivityManager.dispatchDestroy(getActivity().isFinishing());
     }
 }
--- MapFragmentExample/src/com/inazaruk/example/MyMapFragment.java
+++ MapFragmentExample/src/com/inazaruk/example/MyMapFragment.java
@@ -1,27 +1,11 @@
 package com.inazaruk.example;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
+import android.app.Activity;
 
-public class MyMapFragment extends LocalActivityManagerFragment {
-    
-    private TabHost mTabHost;
+public class MyMapFragment extends ActivityHostFragment {
     
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {        
-        View view = inflater.inflate(R.layout.my_map_fragment, container, false);
-        mTabHost = (TabHost)view.findViewById(android.R.id.tabhost);
-        mTabHost.setup(getLocalActivityManager());
-        
-        TabSpec tab = mTabHost.newTabSpec("map")
-                              .setIndicator("map")
-                              .setContent(new Intent(getActivity(), MyMapActivity.class));
-        mTabHost.addTab(tab);        
-        return view;
+    protected Class<? extends Activity> getActivityClass() {
+        return MyMapActivity.class;
     }
 }
